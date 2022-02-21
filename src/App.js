123a774441175869e7c963155ed812e64681b911
Kai Wang <kaiuchihaa@gmail.com>
2022-02-21 00:11:00 -0300

    Exercício
    React: Function Components, uma abordagem moderna

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Typography } from "@material-ui/core";
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import 'fontsource-roboto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container Component="article" maxWidth="sm">
+        <Typography variant="h3" Component='h1' align="center">Formulário de cadastro</Typography>
+        <FormularioCadastro />
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import {TextField, Button, Switch, FormControlLabel} from "@material-ui/core";
+
+function FormularioCadastro() {
+    const [nome, setNome] = useState("");
+  return (
+    <form onSubmit={(event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        console.log(nome);
+        }}>
+        <TextField 
+            value = {nome}
+            onChange={(event) => {
+                setNome(event.target.value);
+            if(nome.length >= 3){
+                    setNome(nome.substring(0, 3));
+                }
+                }}
+            
+            id='nome' 
+            label='Nome' 
+            variant="outlined" 
+            fullWidth 
+            margin="normal" 
+            color='Secondary'/>
+        <TextField 
+            id='Sobrenome' 
+            label='Sobrenome' 
+            variant="outlined" 
+            fullWidth margin="normal" 
+            color='Secondary' />
+            <TextField 
+            id='CPF' 
+            label='CPF' 
+            variant="outlined" 
+            fullWidth 
+            margin="normal"
+            color='Secondary'/>
+        
+        <FormControlLabel 
+            label='Novidades'
+            control={
+                <Switch 
+                name="novidades" 
+                label="novidades" 
+                defaultChecked 
+                color='Secondary'/>}/>
+        <FormControlLabel
+            label='Promoções'
+            control={
+                <Switch 
+                name='Promoções' 
+                label='Promoções' 
+                defaultChecked 
+                color='Secondary'/>}/>
+        
+        
+        <Button type='submite' variant="contained" color='Secondary'>Cadastrar</Button>      
+    </form>
+  );
+}
+
+export default FormularioCadastro;
